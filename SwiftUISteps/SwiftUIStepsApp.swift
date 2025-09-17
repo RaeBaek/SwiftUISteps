e6b09d4dd70b86a8a700c5d5afbe953bffd96caf
RaeBaek <bmp117@naver.com>
2025-09-17 21:18:20 +0900

    Initial Commit

---
+++ SwiftUISteps/SwiftUIStepsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStepsApp.swift
+//  SwiftUISteps
+//
+//  Created by 백래훈 on 9/17/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStepsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
